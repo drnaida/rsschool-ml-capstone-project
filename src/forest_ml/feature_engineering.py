677244387fe2337f5a9cff0014e3c0ab897d94cc
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-05-06 19:39:06 +0300

    Feat: add svd to feature engineering

--- src/forest_ml/feature_engineering.py
+++ src/forest_ml/feature_engineering.py
@@ -1,9 +1,9 @@
+from sklearn.decomposition import TruncatedSVD
 def feature_engineering(dataset, feature_engineering_tech):
     if feature_engineering_tech == '1':
         dataset['Euclidian_Distance_To_Hydrology'] = \
             (dataset['Horizontal_Distance_To_Hydrology'] ** 2 + \
             dataset['Vertical_Distance_To_Hydrology']**2) ** 0.5
-        print(dataset)
         return dataset
     elif feature_engineering_tech == '2':
         columns = [
@@ -17,5 +17,6 @@ def feature_engineering(dataset, feature_engineering_tech):
         for column in columns:
             for degree in range(2, 5):
                 dataset[column + "_" + str(degree)] = dataset[column] ** degree
-        print(dataset)
-        return dataset
+        truncatedSVD = TruncatedSVD(n_components=35)
+        dataset_after_dimensiality_reduction = truncatedSVD.fit_transform(dataset)
+        return dataset_after_dimensiality_reduction
