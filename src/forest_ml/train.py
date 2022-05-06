ad586cdaf20c2e68fd4eeb3ee7aa97f7619beacf
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-05-06 21:53:31 +0300

    feat: save models, parameters and metrics to mlflow

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -1,14 +1,16 @@
 import argparse
 import pathlib
+import numpy as np
+import os
 from .parse_dataset import get_dataset
 from joblib import dump
 from .model_pipeline import create_pipeline
+from .model_pipeline import _params_for_models
 from .feature_engineering import feature_engineering
 from sklearn.model_selection import cross_validate
 
 import mlflow
 import mlflow.sklearn
-from sklearn.metrics import accuracy_score
 def get_params()->dict:
     # arguments needed for model in general
     parser = argparse.ArgumentParser(description='Train the model')
@@ -67,10 +69,20 @@ def train() -> None:
         test_split_ratio=params['test_split_ratio']
     )
     X = feature_engineering(dataset=X, feature_engineering_tech=params['fetengtech'])
-    pipeline = create_pipeline(**params)
-    scoring = {'acc': 'accuracy',
-               'f1_weighted': 'f1_weighted',
-               'roc_auc_ovr': 'roc_auc_ovr'}
-    scores = cross_validate(pipeline, X, y, scoring=scoring)
-    print(scores)
-    dump(pipeline, params['path_save_model'])
+    with mlflow.start_run():
+        pipeline = create_pipeline(**params)
+        scoring = {'acc': 'accuracy',
+                   'f1_weighted': 'f1_weighted',
+                   'roc_auc_ovr': 'roc_auc_ovr'}
+        scores = cross_validate(pipeline, X, y, scoring=scoring)
+        avg_accuracy = np.mean(scores['test_acc'])
+        avg_f1 = np.mean(scores['test_f1_weighted'])
+        avg_roc_auc_ovr = np.mean(scores['test_roc_auc_ovr'])
+        dump(pipeline, params['path_save_model'])
+        mlflow.sklearn.log_model(sk_model=pipeline, artifact_path=os.path.dirname(params['path_save_model']))
+        params = _params_for_models(params)
+        for param in params:
+            mlflow.log_param(param, params[param])
+        mlflow.log_metric("accuracy", avg_accuracy)
+        mlflow.log_metric("f1_weighted", avg_f1)
+        mlflow.log_metric("roc_auc_ovr", avg_roc_auc_ovr)
