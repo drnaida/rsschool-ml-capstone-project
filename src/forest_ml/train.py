a4042b8cb5ce3e4b0fedcc7ca128117edb7c2973
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-05-09 21:31:04 +0300

    fix: rewrite CLI from argparse to click

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -18,153 +18,200 @@ from .nested_cross_validation import nested_cross_validation
 
 import mlflow
 import mlflow.sklearn
+import click
 
-
-def get_params() -> dict:
-    # arguments needed for model in general
-    parser = argparse.ArgumentParser(description="Train the model")
-    parser.add_argument(
-        "--path-to-dataset",
-        type=pathlib.Path,
-        required=True,
-        help="A path to the file with your dataset",
-    )
-    parser.add_argument(
-        "--path-save-model",
-        type=pathlib.Path,
-        required=False,
-        default="models/model.joblib",
-        help="A path where to save the trained model",
-    )
-    parser.add_argument(
-        "--random-state",
-        type=int,
-        required=False,
-        default=42,
-        help="random_state for train test split, model training",
-    )
-    parser.add_argument(
-        "--test-split-ratio",
-        type=int,
-        choices=range(0, 1),
-        required=False,
-        default=0.3,
-        help="Test data ratio, 0.3 by default",
-    )
-    parser.add_argument(
-        "--use-scaler",
-        type=bool,
-        required=False,
-        default=False,
-        help="Whether to use a scaler on data or not, False by default",
-    )
-    parser.add_argument(
-        "--cross-validation-type",
-        type=str,
-        required=False,
-        default='nested',
-        choices=[
+@click.command()
+@click.option(
+    "--path-to-dataset",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path),
+    show_default=True,
+    help="A path to the file with your dataset"
+)
+@click.option(
+    "--path-save-model",
+    default="data/model.joblib",
+    required=False,
+    type=click.Path(dir_okay=False, writable=True, path_type=pathlib.Path),
+    show_default=True,
+    help="A path where to save the trained model"
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+    required=False,
+    help="random_state for train test split, model training"
+)
+@click.option(
+    "--test-split-ratio",
+    default=0.3,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+    required=False,
+    help="Test data ratio, 0.3 by default"
+)
+@click.option(
+    "--use-scaler",
+    default=False,
+    type=bool,
+    show_default=True,
+    required=False,
+    help="Whether to use a scaler on data or not, False by default",
+)
+@click.option(
+    '--cross-validation-type',
+    type=click.Choice(
+        [
             'nested',
             'k-fold'
-        ],
-        help="What type of cross-validation to use",
-    )
-    parser.add_argument(
-        "--model",
-        type=str,
-        required=False,
-        choices=[
+        ]
+    ),
+    default='nested',
+    show_default=True,
+    help="What type of cross-validation to use",
+    required=False
+)
+@click.option(
+    '--model',
+    type=click.Choice(
+        [
             "RandomForestClassifier",
             "LogisticRegression",
             "KNeighborsClassifier",
             "ExtraTreesClassifier",
-        ],
-        default="RandomForestClassifier",
-        help="What machine learning model to use",
-    )
-    parser.add_argument(
-        "--fetengtech",
-        type=str,
-        required=False,
-        choices=["1", "2"],
-        default="1",
-        help="What feature engineering technique to use",
-    )
-
-    # hyperparameters for random forest classifier
-    parser.add_argument(
-        "--max-depth",
-        type=int,
-        required=False,
-        default=None,
-        help="hyperparameter for random forest and extratreeclassifier",
-    )
-    parser.add_argument(
-        "--n-estimators",
-        type=int,
-        required=False,
-        default=None,
-        help="hyperparameter for random forest and extratreeclassifier",
-    )
-    parser.add_argument(
-        "--max-features",
-        type=str,
-        required=False,
-        default=None,
-        help="hyperparameter for random forest and extratreeclassifier",
-    )
-
-    # knn
-    parser.add_argument(
-        "--n-neighbors",
-        type=int,
-        required=False,
-        default=None,
-        help="hyperparameter for knn",
-    )
-    parser.add_argument(
-        "--weights",
-        type=str,
-        required=False,
-        default=None,
-        help="hyperparameter for knn",
-    )
-
-    # logistic regression
-    parser.add_argument(
-        "--max-iter",
-        type=int,
-        required=False,
-        default=None,
-        help="hyperparameter for logistic regression",
-    )
-    parser.add_argument(
-        "--C",
-        type=float,
-        required=False,
-        default=None,
-        help="hyperparameter for logistic regression",
-    )
-    parser.add_argument(
-        "--penalty",
-        type=str,
-        required=False,
-        default=None,
-        help="hyperparameter for logistic regression",
-    )
-    parser.add_argument(
-        "--solver",
-        type=str,
-        required=False,
-        default=None,
-        help="hyperparameter for logistic regression",
-    )
-    arguments = parser.parse_args()
-    return arguments.__dict__
-
-
-def train() -> None:
-    params = dict(filter(lambda x: x[1] is not None, get_params().items()))
+        ]
+    ),
+    default='RandomForestClassifier',
+    show_default=True,
+    help="What machine learning model to use",
+    required=False
+)
+@click.option(
+    '--fetengtech',
+    type=click.Choice(
+        [
+            "1",
+            "2",
+        ]
+    ),
+    default='1',
+    show_default=True,
+    help="What feature engineering technique to use",
+    required=False
+)
+@click.option(
+    "--max-depth",
+    default=None,
+    type=int,
+    show_default=True,
+    required=False,
+    help="hyperparameter for random forest and extratreeclassifier",
+)
+@click.option(
+    "--n-estimators",
+    default=None,
+    type=int,
+    show_default=True,
+    required=False,
+    help="hyperparameter for random forest and extratreeclassifier",
+)
+@click.option(
+    "--max-features",
+    default=None,
+    type=str,
+    show_default=True,
+    required=False,
+    help="hyperparameter for random forest and extratreeclassifier",
+)
+@click.option(
+    "--n-neighbors",
+    default=None,
+    type=int,
+    show_default=True,
+    required=False,
+    help="hyperparameter for knn",
+)
+@click.option(
+    "--weights",
+    default=None,
+    type=str,
+    show_default=True,
+    required=False,
+    help="hyperparameter for knn",
+)
+@click.option(
+    "--max-iter",
+    default=None,
+    type=int,
+    show_default=True,
+    required=False,
+    help="hyperparameter for logistic regression",
+)
+@click.option(
+    "--C",
+    default=None,
+    type=float,
+    show_default=True,
+    required=False,
+    help="hyperparameter for logistic regression",
+)
+@click.option(
+    "--penalty",
+    default=None,
+    type=str,
+    show_default=True,
+    required=False,
+    help="hyperparameter for logistic regression",
+)
+@click.option(
+    "--solver",
+    default=None,
+    type=str,
+    show_default=True,
+    required=False,
+    help="hyperparameter for logistic regression",
+)
+def train(
+    path_to_dataset: pathlib.Path,
+    path_save_model: pathlib.Path,
+    random_state: int,
+    test_split_ratio: float,
+    use_scaler: bool,
+    cross_validation_type: str,
+    model: str,
+    fetengtech: str,
+    max_depth: int,
+    n_estimators: int,
+    max_features: str,
+    n_neighbors: int,
+    weights: str,
+    max_iter: int,
+    c: float,
+    penalty: str,
+    solver: str,
+) -> None:
+    params_list = {
+        "path_to_dataset": path_to_dataset,
+        "path_save_model": path_save_model,
+        "random_state": random_state,
+        "test_split_ratio": test_split_ratio,
+        "use_scaler": use_scaler,
+        "cross_validation_type": cross_validation_type,
+        "model": model,
+        "fetengtech": fetengtech,
+        "max_depth": max_depth,
+        "n_estimators": n_estimators,
+        "max_features": max_features,
+        "n_neighbors": n_neighbors,
+        "weights": weights,
+        "max_iter": max_iter,
+        "C": c,
+        "penalty": penalty,
+        "solver": solver,
+    }
+    params = dict(filter(lambda x: x[1] is not None, params_list.items()))
     print("params", params)
 
     X, y = get_dataset(
--- tests/test_forest_ml.py
+++ tests/test_forest_ml.py
@@ -1,5 +1,25 @@
-from forest_ml import __version__
+from click.testing import CliRunner
+import pytest
+
+from forest_ml.train import train
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+
+def test_error_for_invalid_test_split_ratio(
+    runner: CliRunner
+) -> None:
+    """It fails when test split ratio is greater than 1."""
+    result = runner.invoke(
+        train,
+        [
+            "--random-state",
+            -1,
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--test-split-ratio'" in result.output
