fd1df998bbe13be7d3485931c057c72da30b10d8
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-05-10 00:29:40 +0300

    fix: make successful test work

--- tests/test_forest_ml.py
+++ tests/test_forest_ml.py
@@ -1,5 +1,6 @@
 from click.testing import CliRunner
 import pytest
+import click
 
 from forest_ml.train import train
 
@@ -46,15 +47,17 @@ def test_error_for_invalid_test_split_ratio(runner: CliRunner) -> None:
 
 def test_valid_parameters(runner: CliRunner) -> None:
     """It fails when test split ratio is greater than 1."""
+    runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(
             train,
             [
                 "--path-to-dataset",
-                r"D:\dev\rsschool-ml-capstone-project\data\train.csv",
+                "D:/dev/rsschool-ml-capstone-project/data/train.csv",
                 "--cross-validation-type",
                 "k-fold",
+                "--path-save-model",
+                "D:/dev/rsschool-ml-capstone-project/data/model.joblib"
             ],
         )
-        print(result.output)
         assert result.exit_code == 0
